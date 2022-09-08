a4525f3be891e933848be99f81740548d0bf9858
girardi1994 <30416996+girardi1994@users.noreply.github.com>
2022-09-07 23:07:33 -0300

    Segunda página app

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -9,7 +9,7 @@ import {
 } from "react-native";
 import { Feather, Ionicons } from "@expo/vector-icons";
 import SwiperComponent from "../components/Swiper";
-import Stars from  'react-native-stars';
+import Stars from "react-native-star-rating";
 
 export default function Detail() {
   return (
@@ -19,14 +19,18 @@ export default function Detail() {
       </View>
       <View style={styles.headerContent}>
         <View style={{ width: "65%" }}>
-          <Text>Casa de Praia</Text>
+          <Text style={styles.house}>Casa de Praia</Text>
         </View>
         <View style={{ width: "35%" }}>
           <Text style={styles.rating}>Avaliações</Text>
-          <View style={{alignItems: 'center', flexDirection:'row'}}>
-            <Stars
-            default={4}
-            />
+          <View style={{ alignItems: "center", flexDirection: "row" }}>
+            {[1, 2, 3, 4, 5].map((item, index) => {
+              return index < 5 ? (
+                <Ionicons key={index} name="md-star" color="#eedf11" />
+              ) : (
+                <Ionicons key={index} name="md-star-outline" color="#000" />
+              );
+            })}
           </View>
         </View>
       </View>
@@ -43,5 +47,22 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     height: 340,
     width: "100%",
+  },
+  headerContent: {
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    marginTop: 20,
+  },
+  house: {
+    fontFamily: "Montserrat_700Bold",
+    fontSize: 18,
+    color: "$4f4a4a",
+  },
+  rating: {
+    fontFamily: "Montserrat_500Medium",
+    fontSize: 9,
+    color: "$4f4a4a",
   },
 });
