21f304c9a407106e45e7a7f54dd3e38fb7a45ee6
girardi1994 <30416996+girardi1994@users.noreply.github.com>
2022-09-07 23:34:19 -0300

    Finalizando o projeto

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -34,6 +34,34 @@ export default function Detail() {
           </View>
         </View>
       </View>
+      <Text style={styles.price}>
+        R$ 1.250,00
+      </Text>
+      <Text style={styles.description}>
+        Casa nova uma quadra do mar, lugar seguro e monitorado 24h.
+      </Text>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{paddingHorizontal: 15, marginTop: 35}}>
+<View style={styles.slide}>
+<Image
+source={require('../assets/house6.jpg')}
+style={{width: 90, height: 90, borderRadius: 8}}
+/>
+
+</View>
+<View style={styles.slide}>
+<Image
+source={require('../assets/house5.jpg')}
+style={{width: 90, height: 90, borderRadius: 8}}
+/>
+
+</View>
+<View style={styles.slide}>
+<Image
+source={require('../assets/house2.jpg')}
+style={{width: 90, height: 90, borderRadius: 8}}
+/>
+</View>
+      </ScrollView>
     </View>
   );
 }
@@ -65,4 +93,26 @@ const styles = StyleSheet.create({
     fontSize: 9,
     color: "$4f4a4a",
   },
+  price:{
+    paddingHorizontal: 20,
+    fontFamily:'Montserrat_700Bold',
+    fontSize: 16,
+    color: '#000',
+  },
+  description:{
+    paddingHorizontal: 20,
+    fontFamily:'Montserrat_500Medium',
+    fontSize: 14,
+    color: '#b3aeae',
+    lineHeight: 20,
+    marginTop: 20,
+  },
+  slide:{
+    alignContent:'center',
+    alignItems:'center',
+    backgroundColor: '#fff',
+    height: 90,
+    borderRadius: 8,
+    marginRight: 20,
+  }
 });
